27a7ca798aee567db1fb0a8323c83ff9b6541b27
Daniel Kaplan <dkaplan@localhost.localdomain>
2025-06-11 10:04:33 -0400

    Basic test class for custom dataset #8

--- dinov2/data/loaders.py
+++ dinov2/data/loaders.py
@@ -10,7 +10,7 @@ from typing import Any, Callable, List, Optional, TypeVar
 import torch
 from torch.utils.data import Sampler
 
-from .datasets import ImageNet, ImageNet22k
+from .datasets import ImageNet, ImageNet22k, TestVisionDataset
 from .samplers import EpochSampler, InfiniteSampler, ShardedInfiniteSampler
 
 
@@ -42,6 +42,7 @@ def _make_sample_transform(image_transform: Optional[Callable] = None, target_tr
 
 
 def _parse_dataset_str(dataset_str: str):
+
     tokens = dataset_str.split(":")
 
     name = tokens[0]
@@ -58,6 +59,9 @@ def _parse_dataset_str(dataset_str: str):
             kwargs["split"] = ImageNet.Split[kwargs["split"]]
     elif name == "ImageNet22k":
         class_ = ImageNet22k
+    elif name.lower() == "pathology":
+        class_ = TestVisionDataset
+        print("kwargs", kwargs)
     else:
         raise ValueError(f'Unsupported dataset "{name}"')
 
