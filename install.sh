39bdef848814baf8ba4dcf057010b5c356733461
daniel-z-kaplan <48258016+daniel-z-kaplan@users.noreply.github.com>
2025-08-05 16:40:14 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ source dino_env/bin/activate
 uv pip install -r requirements.txt
 uv pip install -e .
 uv pip install openslide-python openslide-bin opencv-python scikit-image einops matplotlib opencv-python
+uv pip install 'kaiko-eva[vision]'
+cp _utils.py ./dino_env/lib/python3.11/site-packages/eva/core/models/wrappers/
