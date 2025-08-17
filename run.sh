9d33027838a38145ec1ac1ce8d049deadd723d00
Paul Scotti <paulpauzle@gmail.com>
2025-08-17 14:48:08 +0000

    configured for s3 instead of local

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 source .venv/bin/activate
-CUDA_VISIBLE_DEVICES=2,3 torchrun --master_port=30002 --nproc_per_node=2 dinov2/train/train.py --config-file ./dinov2/configs/train/vits14_reg4.yaml --output-dir ./output_pretrained_on_test
-
+CUDA_VISIBLE_DEVICES=0 torchrun --master_port=34001 --nproc_per_node=1 dinov2/train/train.py --config-file ./dinov2/configs/train/vits14_reg4.yaml --output-dir ./output_pretrained_on_test
